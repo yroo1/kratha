cef382efafb5fb57b4089df75615cd5b2fd7f36c
yroo <purnamaaleron7@gmail.com>
2026-05-24 18:00:39 +0700

    Create KrathaPal.java

---
+++ src/kratha/graphics/KrathaPal.java
@@ -0,0 +1,10 @@
+package kratha;
+
+import arc.graphics.Color;
+
+public class GenesisPal{
+    public static Color
+    krathiteDark = Color.valueOf("51436B"),
+    krathite = Color.valueOf("897090"),
+    krathiteLight = Color.valueOf("B3A0B6");
+}

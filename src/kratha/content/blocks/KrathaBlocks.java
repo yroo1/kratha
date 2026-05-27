835df9f670b250eb44c8610743b956b08d2052d0
yroo <purnamaaleron7@gmail.com>
2026-05-27 20:47:39 +0700

    Update KrathaBlocks.java

--- src/kratha/content/blocks/KrathaBlocks.java
+++ src/kratha/content/blocks/KrathaBlocks.java
@@ -2,6 +2,7 @@ package kratha.content.blocks;
 
 public class KrathaBlocks{
     public static void load(){
+        KrathaEnv.load();
         KrathaDistribution.load();
         KrathaStorage.load();
     }

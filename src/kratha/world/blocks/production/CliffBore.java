7295980d611e2474a15d574a2372c54a531428d8
yroo <purnamaaleron7@gmail.com>
2026-05-30 21:46:14 +0700

    Create CliffBore.java

---
+++ src/kratha/world/blocks/production/CliffBore.java
@@ -0,0 +1,124 @@
+package kratha.world.blocks.production;
+
+import arc.Core;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.*;
+import arc.util.*;
+import arc.math.geom.*;
+import mindustry.world.blocks.production.*;
+import mindustry.gen.Building;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.Tile;
+import mindustry.core.*;
+import mindustry.entities.units.*;
+
+import static mindustry.Vars.*;
+
+public class CliffDrill extends BeamDrill {
+    public TextureRwgion topRegion1, topRegion2;
+    public CliffDrill(String name){
+         super(name);
+    }
+    @Override
+    public void load(){
+        super.load();
+        topRegion1=Core.atlas.find(name+"-top1");
+        topRegion2=Core.atlas.find(name+"-top2");
+    }
+    @Override
+    public void drawPlanRegion(BuildPlan plan, Eachable<BuildPlan> list){
+        Draw.rect(region, plan.drawx(), plan.drawy());
+        Draw.rect((plan.rotation>1?topRegion2:topRegion1), plan.drawx(), plan.drawy(), plan.rotation*90);
+    }
+    @Override
+    public TextureRegion[] icons(){
+        return new TextureRegion[]{region, topRegions[0]};
+    }
+    public class CliffDrillBuild extends BeamDrillBuild {
+        @Override
+        public void draw(){
+            Draw.rect(block.region, x, y);
+            Draw.rect((plan.rotation>1?topRegion2:topRegion1), x, y, rotdeg()));
+
+            if(isPayload()) return;
+
+            var dir = Geometry.d4(rotation);
+            int ddx = Geometry.d4x(rotation + 1), ddy = Geometry.d4y(rotation + 1);
+
+            for(int i = 0; i < size; i++){
+                Tile face = facing[i];
+                if(face != null){
+                    Point2 p = lasers[i];
+                    float lx = face.worldx() - (dir.x/2f)*tilesize, ly = face.worldy() - (dir.y/2f)*tilesize;
+
+                    float width = (laserWidth + Mathf.absin(Time.time + i*5 + (id % 9)*9, glowScl, pulseIntensity)) * warmup;
+
+                    Draw.z(Layer.power - 1);
+                    Draw.mixcol(glowColor, Mathf.absin(Time.time + i*5 + id*9, glowScl, glowIntensity));
+                    if(Math.abs(p.x - face.x) + Math.abs(p.y - face.y) == 0){
+                        Draw.scl(width);
+
+                        if(boostWarmup < 0.99f){
+                            Draw.alpha(1f - boostWarmup);
+                            Draw.rect(laserCenter, lx, ly);
+                        }
+
+                        if(boostWarmup > 0.01f){
+                            Draw.alpha(boostWarmup);
+                            Draw.rect(laserCenterBoost, lx, ly);
+                        }
+
+                        Draw.scl();
+                    }else{
+                        float lsx = (p.x - dir.x/2f) * tilesize, lsy = (p.y - dir.y/2f) * tilesize;
+
+                        if(boostWarmup < 0.99f){
+                            Draw.alpha(1f - boostWarmup);
+                            Drawf.laser(laser, laserEnd, lsx, lsy, lx, ly, width);
+                        }
+
+                        if(boostWarmup > 0.001f){
+                            Draw.alpha(boostWarmup);
+                            Drawf.laser(laserBoost, laserEndBoost, lsx, lsy, lx, ly, width);
+                        }
+                    }
+                    Draw.color();
+                    Draw.mixcol();
+
+                    Draw.z(Layer.effect);
+                    Lines.stroke(warmup);
+                    rand.setState(i, id);
+                    Color col = face.wallDrop().color;
+                    Color spark = Tmp.c3.set(sparkColor).lerp(boostHeatColor, boostWarmup);
+                    for(int j = 0; j < sparks; j++){
+                        float fin = (Time.time / sparkLife + rand.random(sparkRecurrence + 1f)) % sparkRecurrence;
+                        float or = rand.range(2f);
+                        Tmp.v1.set(sparkRange * fin, 0).rotate(rotdeg() + rand.range(sparkSpread));
+
+                        Draw.color(spark, col, fin);
+                        float px = Tmp.v1.x, py = Tmp.v1.y;
+                        if(fin <= 1f) Lines.lineAngle(lx + px + or * ddx, ly + py + or * ddy, Angles.angle(px, py), Mathf.slope(fin) * sparkSize);
+                    }
+                    Draw.reset();
+                }
+            }
+
+            if(glowRegion.found()){
+                Draw.z(Layer.blockAdditive);
+                Draw.blend(Blending.additive);
+                Draw.color(Tmp.c1.set(heatColor).lerp(boostHeatColor, boostWarmup), warmup * (heatColor.a * (1f - heatPulse + Mathf.absin(heatPulseScl, heatPulse))));
+                Draw.rect(glowRegion, x, y, rotdeg());
+                Draw.blend();
+                Draw.color();
+            }
+
+            Draw.blend();
+            Draw.reset();
+        }
+
+    }
+}

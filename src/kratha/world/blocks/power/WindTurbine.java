5bcee273535950960f5647d49b0798ef69865b1f
yroo <purnamaaleron7@gmail.com>
2026-05-31 11:59:53 +0700

    Update WindTurbine.java

--- src/kratha/world/blocks/power/WindTurbine.java
+++ src/kratha/world/blocks/power/WindTurbine.java
@@ -21,7 +21,7 @@ import mindustry.content.Blocks;
 import static mindustry.Vars.*;
 
 public class WindTurbine extends PowerGenerator{
-    public int range = 10;
+    public int range = 12;
     public DrawBlock drawer = new DrawDefault();
     public Color baseColor = Pal.accent;
     public Color obstructionColor = Pal.remove;
@@ -114,7 +114,7 @@ public class WindTurbine extends PowerGenerator{
             for(int xm = -frange+1;xm<=frange;xm++){
                 for(int ym = -frange+1;ym<=frange;ym++){
                     Tile other = tile.nearby(xm,ym);
-                    if(other.solid()) {
+                    if(other!=null&&other.solid()) {
                         rcount++;
                     }
                 }
@@ -142,7 +142,7 @@ public class WindTurbine extends PowerGenerator{
             for(int xm = -frange+1;xm<=frange;xm++){
                 for(int ym = -frange+1;ym<=frange;ym++){
                     Tile other = tile.nearby(xm,ym);
-                    if(other.solid()&&other.build!=this) {
+                    if(other!=null&&other.solid()&&other.build!=this) {
                         Drawf.selected(other.x, other.y, Blocks.router, obstructionColor);
                     }
                 }

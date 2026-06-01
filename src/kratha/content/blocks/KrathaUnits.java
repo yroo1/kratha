8f33623ed0fc48a2fe53bca814c29c7b8c4171b1
yroo <purnamaaleron7@gmail.com>
2026-06-01 09:13:40 +0700

    Create KrathaUnits.java

---
+++ src/kratha/content/blocks/KrathaUnits.java
@@ -0,0 +1,33 @@
+package kratha.content.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.units.*;
+import mindustry.world.draw.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import kratha.content.KrathaItems;
+import kratha.content.*;
+import mindustry.content.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class KrathaUnits {
+    public static Block
+            rockboredFactory;
+    public static void load() {
+        {
+            {
+                rockboredFactory = new UnitFactory("mech-assembler"){{
+                    requirements(Category.units, with(KrathaItems.guartz, 120, KrathaItems.krathite,100, KrathaItems.spurstone,75));
+                    plans.add(new UnitPlan(KrathaUnitTypes.keris, 15 * 60f, with(KrathaItems.guartz, 15,KrathaItems.spurstone, 10)));
+                    consumePower(90/60f);
+                    size = 4;
+                }};
+            }
+        }
+    }
+}

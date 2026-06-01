406f98d6de4587d7aac17f73ac295ddc907df287
yroo <purnamaaleron7@gmail.com>
2026-06-01 18:04:34 +0700

    Create KrathaTechTree.java

---
+++ src/kratha/content/KrathaTechTree.java
@@ -0,0 +1,54 @@
+package kratha.content;
+
+import arc.struct.*;
+import kratha.content.KrathaPlanets;
+import kratha.content.blocks.*;
+import kratha.content.*;
+import mindustry.game.Objectives;
+import mindustry.type.Item;
+
+import static kratha.content.KrathaSectorPresets.*;
+import static mindustry.Vars.content;
+import static mindustry.content.TechTree.*;
+
+public class DeterraTechTree {
+    public static void load(){
+        KrathaPlanets.kratha.techTree = nodeRoot("KRATHA", DeterraStorage.coreProtocol, () -> {
+            node(KrathaItems.krathite, Seq.with(new Objectives.Produce(KrathaItems.krathite)), () -> {
+                node(KrathaItems.guartz, Seq.with(new Objectives.Produce(KrathaItems.guartz)), () ->{
+                    node(KrathaItems.terrasand, Seq.with(new Objectives.Produce(KrathaItems.terrasand)), () ->{
+                        node(KrathaItems.spurstone, Seq.with(new Objectives.Produce(KrathaItems.spurstone)), () ->{
+                    
+                        });
+                    });
+                });
+            });
+            node(KrathaDistribution.itemTube, () -> {
+                node(KrathaDistribution.splitter, () -> {
+                   
+                });
+                node(KrathaDistribution.itemOverpass);
+            });
+            node(KrathaProduction.thermicDrill, () -> {
+                node(KrathaProduction.laserBore, () -> {
+                        
+                });
+                node(KrathaProduction.cliffBore, () -> {
+                        
+                });
+            });
+            node(KrathaProduction.spurstoneSmelter, () -> {
+            });
+            
+            node(KrathaPower.relay, () -> {
+                node(KrathaPower.windTurbine, () -> {
+                    
+                });
+            });
+            
+            node(KrathaSectorPresets.theFountain, () -> {
+                
+            });
+        });
+    }
+}

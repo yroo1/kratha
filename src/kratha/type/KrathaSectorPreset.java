a53c23370fa483130e6fd3c2a4d15cc095739f32
yroo <purnamaaleron7@gmail.com>
2026-06-01 18:09:50 +0700

    Create KrathaSectorPreset.java

---
+++ src/kratha/type/KrathaSectorPreset.java
@@ -0,0 +1,13 @@
+package kratha.content;
+
+import mindustry.type.*;
+import mindustry.ctype.*;
+
+//i dont know why i made this
+public class KrathaSectorPreset extends SectorPreset{
+    public KrathaSector sector;
+
+    public KrathaSectorPreset(String name, Planet planet, int sector){
+        super(name, planet, sector);
+    }
+}

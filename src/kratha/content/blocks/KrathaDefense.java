b2665b33e84ff9304939a09ecd8af28d6d7d53de
yroo <purnamaaleron7@gmail.com>
2026-06-03 20:18:16 +0700

    Create KrathaDefense.java

---
+++ src/kratha/content/blocks/KrathaDefense.java
@@ -0,0 +1,29 @@
+package kratha.content.blocks;
+
+import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+import mindustry.content.*;
+import mindustry.gen.*;
+import kratha.content.KrathaItems;
+import kratha.graphics.KrathaPal;
+
+import static mindustry.type.ItemStack.with;
+
+public class KrathaDefense{
+    public static Block
+    radar;
+  
+    public static void load(){
+         radar = new Radar("radar"){{
+            requirements(Category.effect, BuildVisibility.fogOnly, with(KrathaItems.krathite, 30, KrathaItems.guartz, 50, KrathaItems.spurstone, 15));
+            outlineColor = KrathaPal.krathaOutline;
+            fogRadius = 40;
+            researchCost = with(KrathaItems.krathite, 100, KrathaItems.guartz, 150, KrathaItems.spurstone, 50);
+
+            consumePower(30/60f);
+        }};
+    }
+}

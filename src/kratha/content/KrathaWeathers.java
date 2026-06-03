8df4c3ab875562b2513251e3439b31424f915cfd
yroo <purnamaaleron7@gmail.com>
2026-06-03 19:15:08 +0700

    Update KrathaWeathers.java

--- src/kratha/content/KrathaWeathers.java
+++ src/kratha/content/KrathaWeathers.java
@@ -10,9 +10,26 @@ import mindustry.type.weather.*;
 import kratha.graphics.KrathaPal;
 
 public class KrathaWeathers {
-    //public static Weather krathagenFog;
+    public static Weather krathagenFog;
 
     public static void load(){
-        //uhh
+        krathagenFog = new ParticleWeather("krathagen-fog"){{
+            color = noiseColor = KrathaPal.krathagenLight;
+            particleRegion = "circle-small";
+            drawNoise = true;
+            statusGround = false;
+            useWindVector = true;
+            sizeMax = 4f;
+            sizeMin = 2f;
+            minAlpha = 0.1f;
+            maxAlpha = 0.8f;
+            density = 2000f;
+            baseSpeed = 4.5f;
+            attrs.set(Attribute.light, -0.15f);
+            opacityMultiplier = 0.5f;
+            force = 0.1f;
+            sound = Sounds.wind;
+            soundVol = 0.7f;
+        }};
     }
 }

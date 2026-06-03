e5fe4335f856ba3dbcb071c5e006ca27568e3a44
yroo <purnamaaleron7@gmail.com>
2026-06-03 19:09:54 +0700

    Create KrathaWeathers.java

---
+++ src/kratha/content/KrathaWeathers.java
@@ -0,0 +1,18 @@
+package kratha.content;
+
+import arc.graphics.Color;
+import arc.util.Time;
+import mindustry.content.*;
+import mindustry.gen.*;
+import mindustry.world.meta.*;
+import mindustry.type.Weather;
+import mindustry.type.weather.*;
+import kratha.graphics.KrathaPal;
+
+public class KrathaWeathers {
+    //public static Weather krathagenFog;
+
+    public static void load(){
+        //uhh
+    }
+}

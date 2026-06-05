be838e56a2167b7d4e73b5a8206a0abf1eda0451
yroo <purnamaaleron7@gmail.com>
2026-06-05 11:26:07 +0700

    Create KrathaFx.java

---
+++ src/kratha/content/KrathaFx.java
@@ -0,0 +1,33 @@
+package kratha.content;
+
+import arc.Core;
+import arc.graphics.Color;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.Vec2;
+import arc.util.Time;
+import mindustry.entities.Effect;
+import mindustry.gen.Bullet;
+import mindustry.graphics.*;
+import kratha.graphics.KrathaPal;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.randLenVectors;
+
+public class KrathaFx{
+    public static final Effect
+    bulbPop = new Effect(10f, 80f, e -> {
+        color(KrathaPal.guartz, KrathaPal.guartzLight, e.fin());
+        stroke(e.fout() * 3f);
+        Lines.circle(e.x, e.y, e.fin() * 32f);
+    }),
+    kerisShoot = new Effect(12f, e -> {
+        color(Color.white, KrathaPal.guartzLight, e.fin());
+        stroke(e.fout() * 1.2f + 0.5f);
+
+        randLenVectors(e.id, 7, 25f * e.finpow(), e.rotation, 50f, (x, y) -> {
+            lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fin() * 5f + 2f);
+        });
+    }),
+}

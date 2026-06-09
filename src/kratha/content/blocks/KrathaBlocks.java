12f49f96df9bfb9003142f0f344717431af319c7
yroo <purnamaaleron7@gmail.com>
2026-06-09 19:00:13 +0700

    Update KrathaBlocks.java

--- src/kratha/content/blocks/KrathaBlocks.java
+++ src/kratha/content/blocks/KrathaBlocks.java
@@ -4,6 +4,7 @@ import kratha.content.terraplasm.Terraplasm;
 public class KrathaBlocks{
     public static void load(){
         KrathaEnv.load();
+        KrathaTurrets.load();
         KrathaDefense.load();
         KrathaProduction.load();
         KrathaDistribution.load();

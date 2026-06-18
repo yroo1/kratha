c6cd48ec55608cd0947b70a2f607306caee67ec0
yroo <purnamaaleron7@gmail.com>
2026-06-18 18:38:43 +0700

    Update LiquidTube.java

--- src/kratha/world/blocks/liquid/LiquidTube.java
+++ src/kratha/world/blocks/liquid/LiquidTube.java
@@ -14,6 +14,7 @@ import mindustry.ui.*;
 import mindustry.world.*;
 import mindustry.world.Tile;
 import mindustry.entities.units.BuildPlan;
+import kratha.content.KrathaLiquid;
 
 import static mindustry.Vars.*;
 
@@ -29,6 +30,7 @@ public class LiquidTube extends Conduit {
     @Override
     public void load(){
         super.load();
+        rotBridgeReplacement = KrathaLiquid.liquidOverpass;
         
         int y = 0;
         for(int cy = 0; cy < 4; cy++, y+=32){

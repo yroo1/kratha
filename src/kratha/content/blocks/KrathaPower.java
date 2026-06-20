8908119103f874fa92140a4f734cc091acfd504f
yroo <purnamaaleron7@gmail.com>
2026-06-20 18:11:38 +0700

    Update KrathaPower.java

--- src/kratha/content/blocks/KrathaPower.java
+++ src/kratha/content/blocks/KrathaPower.java
@@ -53,7 +53,7 @@ public class KrathaPower{
             hasLiquids = true;
             consumeLiquid(KrathaLiquids.steam,12f/60);
             outputLiquid = new LiquidStack(KrathaLiquids.watra,4f/60);
-            drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidRegion(){{drawLiquid=KrathaLiquids.watra;}}, new DrawBlurSpin("-rotator", 5), new DrawDefault(), new DrawGlowRegion());
+            drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidRegion(){{drawLiquid=KrathaLiquids.watra;}}, new DrawBlurSpin("-rotator", 5), new DrawDefault(), new DrawGlowRegion(){{color=KrathaPal.steamLight;}});
         }};
         
 

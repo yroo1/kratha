4c8dc4c5de55075464220043d3210b6a5bc91374
yroo <purnamaaleron7@gmail.com>
2026-06-24 15:49:05 +0700

    Create DPInfoBlock.java

---
+++ src/kratha/world/blocks/terraplasm/DPInfoBlock.java
@@ -0,0 +1,12 @@
+package kratha.world.blocks.terraplasm;
+
+import mindustry.world.blocks.logic.MessageBlock;
+
+public class DPInfoBlock extends MessageBlock {
+    public DPInfoBlock(String name){
+        super(name);
+    }
+
+    public class DPInfoBuild extends MessageBuild {
+    }
+ }     

8a76f4d7a4383d1388093012a8ae2f8df6d3a04c
yroo <purnamaaleron7@gmail.com>
2026-06-25 15:42:26 +0700

    Update MarkerOverlay.java

--- src/kratha/world/blocks/environment/MarkerOverlay.java
+++ src/kratha/world/blocks/environment/MarkerOverlay.java
@@ -8,7 +8,7 @@ import mindustry.world.*;
 import mindustry.graphics.*;
 import mindustry.world.blocks.environment.OverlayFloor;
 import kratha.content.terraplasm.Terraplasm;
-import kratha.world.blocks.terraplasm.Root;
+import kratha.world.blocks.terraplasm.*;
 
 import static mindustry.Vars.*;
 
@@ -20,7 +20,7 @@ public class MarkerOverlay extends OverlayFloor{
     @Override
     public void drawBase(Tile tile){
         if(Terraplasm.root instanceof Root r){
-            if(r.displayMarkers)Draw.rect(region, tile.worldx(), tile.worldy());
+            if(r.showMarkers)Draw.rect(region, tile.worldx(), tile.worldy());
         }
     }
 }

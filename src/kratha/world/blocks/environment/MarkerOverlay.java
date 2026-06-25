5495799f1114dd517a18b53c75e783605e92e480
yroo <purnamaaleron7@gmail.com>
2026-06-25 10:30:30 +0700

    Update and rename LargeOverlayFloor.java to MarkerOverlay.java

--- src/kratha/world/blocks/environment/MarkerOverlay.java
+++ src/kratha/world/blocks/environment/MarkerOverlay.java
@@ -7,13 +7,12 @@ import mindustry.world.*;
 import mindustry.graphics.*;
 import mindustry.world.blocks.environment.OverlayFloor;
 
-public class LargeOverlayFloor extends OverlayFloor{
-    public LargeOverlayFloor(String name){
+public class MarkerOverlay extends OverlayFloor{
+    public MarkerOverlay(String name){
         super(name);
     }
     @Override
     public void drawBase(Tile tile){
-        Draw.z(Layer.floor+0.01f);
-        Draw.rect(variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], tile.worldx(), tile.worldy());
+        //no
     }
 }

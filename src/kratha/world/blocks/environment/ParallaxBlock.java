f49da0d66cc4fec0ec8edf549919ffd5eed756f2
yroo <purnamaaleron7@gmail.com>
2026-06-26 11:45:28 +0700

    Update ParallaxBlock.java

--- src/kratha/world/blocks/environment/ParallaxBlock.java
+++ src/kratha/world/blocks/environment/ParallaxBlock.java
@@ -23,8 +23,13 @@ public class ParallaxBlock extends TallBlock{
         Draw.z(Layer.floor+(parallaxAmount>0?0.01f:-0.01f));
         float camoffX=(tile.worldx()-Core.camera.position.x)*((parallaxAmount)/Core.camera.width);
         float camoffY=(tile.worldy()-Core.camera.position.y)*((parallaxAmount)/Core.camera.width);
+        float scl=parallaxAmount/Core.camera.width;
+        float sx=x-scale*0.5f;
+        float sy=y-scale*0.5f;
+        Draw.scl(scale,scale)
         Draw.rect(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,
-            tile.worldx()+camoffX, tile.worldy()+camoffY);
+            tile.worldx()+camoffX+sx, tile.worldy()+camoffY+sy);
+        Draw.scl(1,1);
     }
     @Override
     public boolean synthetic(){

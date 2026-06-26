e51a3ccbbad88f8ab347dc5f7e6dd64e1da9a8bf
yroo <purnamaaleron7@gmail.com>
2026-06-26 12:32:25 +0700

    Update ParallaxBlock.java

--- src/kratha/world/blocks/environment/ParallaxBlock.java
+++ src/kratha/world/blocks/environment/ParallaxBlock.java
@@ -12,12 +12,17 @@ import mindustry.world.blocks.environment.TallBlock;
 import static mindustry.Vars.*;
 
 public class ParallaxBlock extends TallBlock{
-  public float parallaxAmount = -100;
+    public float parallaxAmount = -100;
+    public float[] vertices = new float[24];
     public ParallaxBlock(String name){
         super(name);
         forceDark = false;
         hasShadow = false;
         solid = false;
+        for(int i = 0; i < 4; i++){
+            vertices[i * 6 + 2] = Color.white.toFloatBits();
+            vertices[i * 6 + 5] = Color.clearFloatBits;
+        }
     }
     @Override
     public void drawBase(Tile tile){
@@ -32,14 +37,20 @@ public class ParallaxBlock extends TallBlock{
         float camoffX4=(tile.worldx()+tilesize/2-Core.camera.position.x)*((parallaxAmount)/Core.camera.width);
         float camoffY4=(tile.worldy()+tilesize/2-Core.camera.position.y)*((parallaxAmount)/Core.camera.width);
 
-        float c = Color.toFloatBits(1,1,1,1);
-        Draw.quad(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,
-            tile.worldx()-tilesize/2, tile.worldy()-tilesize/2, c,
-            tile.worldx()+tilesize/2, tile.worldy()-tilesize/2, c,
-            tile.worldx()+tilesize/2, tile.worldy()+tilesize/2, c,
-            tile.worldx()-tilesize/2, tile.worldy()+tilesize/2, c);
-        
+        setPos(0,tile.worldx()-tilesize/2,tile.worldy()-tilesize/2);
+        setPos(1,tile.worldx()+tilesize/2,tile.worldy()-tilesize/2);
+        setPos(2,tile.worldx()+tilesize/2,tile.worldy()+tilesize/2);
+        setPos(3,tile.worldx()-tilesize/2,tile.worldy()+tilesize/2);
+        Draw.vert(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,, vertices, 0, vertices.length);
     }
+  
+    private void setPos(int i, float x, float y){
+        if(i >= 0 && i < 4){
+            if(!Float.isNaN(x)) vertices[i * 6] = x * tilesize;
+            if(!Float.isNaN(y)) vertices[i * 6 + 1] = y * tilesize;
+        }
+    }
+  
     @Override
     public boolean synthetic(){
         return true;

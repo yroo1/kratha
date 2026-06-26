477f448c3124f727108bc831fadfb9da93e07f67
yroo <purnamaaleron7@gmail.com>
2026-06-26 10:08:37 +0700

    Update ParallaxFloor.java

--- src/kratha/world/blocks/environment/ParallaxFloor.java
+++ src/kratha/world/blocks/environment/ParallaxFloor.java
@@ -6,25 +6,22 @@ import arc.math.*;
 import arc.util.*;
 import mindustry.world.*;
 import mindustry.graphics.Layer;
-import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.TallBlock;
 
 import static mindustry.Vars.*;
 
-public class ParallaxFloor extends Floor{
+public class ParallaxBlock extends TallBlock{
   public float parallaxAmount = -100;
-    public ParallaxFloor(String name){
+    public ParallaxBlock(String name){
         super(name);
+        forceDark = false;
     }
     @Override
-    public void drawMain(Tile tile){
+    public void drawBase(Tile tile){
         Draw.z(Layer.floor+(parallaxAmount>0?0.01f:-0.01f));
         float camoffX=(tile.worldx()-Core.camera.position.x)*((parallaxAmount)/Core.camera.width);
         float camoffY=(tile.worldy()-Core.camera.position.y)*((parallaxAmount)/Core.camera.width);
         Draw.rect(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,
             tile.worldx()+camoffX, tile.worldy()+camoffY);
-    }
-    @Override
-    public boolean updateRender(Tile tile){
-        return true;
     }
 }

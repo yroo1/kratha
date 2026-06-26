1663a64f7b215e29f09b764c82d8cc064584772f
yroo <purnamaaleron7@gmail.com>
2026-06-26 16:08:58 +0700

    Create ParallaxFloor.java

---
+++ src/kratha/world/blocks/environment/ParallaxFloor.java
@@ -0,0 +1,21 @@
+package kratha.world.blocks.environment;
+
+import arc.Core;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import mindustry.game.*;
+import mindustry.world.*;
+import mindustry.graphics.*;
+import mindustry.world.blocks.environment.Floor;
+
+import static mindustry.Vars.*;
+
+public class ParallaxFloor extends Floor{
+    public ParallaxFloor(String name){
+        super(name);
+    }
+    @Override
+    public void drawBase(Tile tile){
+        
+    }
+}

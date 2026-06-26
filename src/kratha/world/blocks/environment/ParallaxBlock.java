d937a7f7e12e797ce6efee278966665a3c2e7b36
yroo <purnamaaleron7@gmail.com>
2026-06-26 13:53:11 +0700

    Update ParallaxBlock.java

--- src/kratha/world/blocks/environment/ParallaxBlock.java
+++ src/kratha/world/blocks/environment/ParallaxBlock.java
@@ -26,35 +26,38 @@ public class ParallaxBlock extends TallBlock{
     }
     @Override
     public void drawBase(Tile tile){
+        float p = parallaxAmount/Core.camera.width;
+        float cx = Core.camera.position.x, cy = Core.camera.position.y;
+        
         float s = tilesize/2f;
         float x = tile.worldx(), y = tile.worldy();
         float c = Color.white.toFloatBits();
         float mc = Color.clearFloatBits;
         TextureRegion reg = variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region;
 
-        verts[0] = x - s;
-        verts[1] = y - s;
+        verts[0] = x - s + (x-s-cx)*p;
+        verts[1] = y - s + (y-s-cy)*p;
         verts[2] = c;
         verts[3] = reg.u;
         verts[4] = reg.v2;
         verts[5] = mc;
 
-        verts[6] = x + s;
-        verts[7] = y - s;
+        verts[6] = x + s + (x+s-cx)*p;
+        verts[7] = y - s + (y-s-cy)*p;
         verts[8] = c;
         verts[9] = reg.u2;
         verts[10] = reg.v2;
         verts[11] = mc;
 
-        verts[12] = x + s;
-        verts[13] = y + s;
+        verts[12] = x + s + (y+s-cx)*p;
+        verts[13] = y + s + (y+s-cy)*p;
         verts[14] = c;
         verts[15] = reg.u2;
         verts[16] = reg.v;
         verts[17] = mc;
 
-        verts[18] = x - s;
-        verts[19] = y + s;
+        verts[18] = x - s + (x-s-cx)*p;
+        verts[19] = y + s + (y+s-cy)*p;
         verts[20] = c;
         verts[21] = reg.u;
         verts[22] = reg.v;

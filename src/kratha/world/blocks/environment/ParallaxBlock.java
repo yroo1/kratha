1dae06e06867e16c7b91dcc89c5554d5a2e79097
yroo <purnamaaleron7@gmail.com>
2026-06-27 09:38:11 +0700

    Update ParallaxBlock.java

--- src/kratha/world/blocks/environment/ParallaxBlock.java
+++ src/kratha/world/blocks/environment/ParallaxBlock.java
@@ -18,7 +18,7 @@ public class ParallaxBlock extends TallBlock{
     public int depthFlag;
     public TextureRegion[] wallRegions = new TextureRegion[6];
 
-    private final float parallaxMultiplier = 1f/1000;
+    private final float parallaxMultiplier = 1f/2000;
     public ParallaxBlock(String name){
         super(name);
         forceDark = false;

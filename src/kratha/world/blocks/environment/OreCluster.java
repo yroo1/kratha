bbcce3005007d28db89c6728d0d574a1309b798e
yroo <purnamaaleron7@gmail.com>
2026-06-29 20:31:15 +0700

    Update OreCluster.java

--- src/kratha/world/blocks/environment/OreCluster.java
+++ src/kratha/world/blocks/environment/OreCluster.java
@@ -23,23 +23,6 @@ public class OreCluster extends Block{
     public float shadowLayer = Layer.power - 1;
     public float rotationRand = 20f;
     public float shadowAlpha = 0.6f;
-    public static final Point2[] offsets = {
-        new Point2(0, 0),
-        new Point2(1, 0),
-        new Point2(1, 1),
-        new Point2(0, 1),
-        new Point2(-1, 1),
-        new Point2(-1, 0),
-        new Point2(-1, -1),
-        new Point2(0, -1),
-        new Point2(1, -1),
-    };
-
-    static{
-        for(var p : offsets){
-            p.sub(1, 1);
-        }
-    }
 
     public OreCluster(String name){
         super(name);
@@ -47,33 +30,7 @@ public class OreCluster extends Block{
         solid = true;
         clipSize = 90;
         customShadow = true;
-    }
-
-    @Override
-    public void drawShadow(Tile tile){
-
-    }
-
-    @Override
-    public TextureRegion[] icons(){
-        return variants == 0 ? super.icons() : new TextureRegion[]{Core.atlas.find(name + "1")};
-    }
-
-    public boolean isCenter(Tile tile){
-        Tile topRight = tile.nearby(1, 1);
-        return topRight != null && topRight.block() == tile.block() && checkAdjacent(topRight);
-    }
-    
-    //note that only the top right tile works for this; render order reasons.
-    //yes anuke
-    public boolean checkAdjacent(Tile tile){
-        for(var point : offsets){
-            Tile other = Vars.world.tile(tile.x + point.x, tile.y + point.y);
-            if(other == null || other.block() != this){
-                return false;
-            }
-        }
-        return true;
+        size = 3;
     }
 
     public class OreClusterBuild extends Building{

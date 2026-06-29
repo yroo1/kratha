6a7705d9a9921c2ac9f1abb43bcc544fc6a1ac6f
yroo <purnamaaleron7@gmail.com>
2026-06-29 21:44:49 +0700

    Update OreClusterDrill.java

--- src/kratha/world/blocks/production/OreClusterDrill.java
+++ src/kratha/world/blocks/production/OreClusterDrill.java
@@ -151,7 +151,7 @@ public class OreClusterDrill extends Block{
             if(other.block == KrathaEnv.guartzCluster){
                 link = other.pos();
                 return false;
-            }else if (other == this{
+            }else if (other == this){
                 link = -1;
                 return false;
             }

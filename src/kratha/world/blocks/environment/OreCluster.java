03c96ccf9bbad335dccefcd30fc8d9a9513ce141
yroo <purnamaaleron7@gmail.com>
2026-06-29 21:27:57 +0700

    Update OreCluster.java

--- src/kratha/world/blocks/environment/OreCluster.java
+++ src/kratha/world/blocks/environment/OreCluster.java
@@ -39,18 +39,18 @@ public class OreCluster extends Block{
     }
 
     @Override
-    public void drawBase(){
+    public void drawBase(Tile tile){
         float rot = Mathf.randomSeedRange(tile.pos() + 1, rotationRand);
 
         Draw.z(shadowLayer);
         Draw.color(0f, 0f, 0f, shadowAlpha);
         Draw.rect(variants > 0 ? variantShadowRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantShadowRegions.length - 1))] : customShadowRegion,
-        x + shadowOffset, y + shadowOffset, rot);
+        tile.worldx() + shadowOffset, tile.worldy() + shadowOffset, rot);
 
         Draw.color();
 
         Draw.z(layer);
-        Draw.rect(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,x, y, rot);
+        Draw.rect(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region, tile.worldx(), tile.worldy(), rot);
     }
 
     public class OreClusterBuild extends Building{

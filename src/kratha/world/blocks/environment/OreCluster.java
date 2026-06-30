f40f16163a2c2d278fc812d0bbec0b3c8f935dde
yroo <purnamaaleron7@gmail.com>
2026-06-30 18:03:49 +0700

    Update OreCluster.java

--- src/kratha/world/blocks/environment/OreCluster.java
+++ src/kratha/world/blocks/environment/OreCluster.java
@@ -48,7 +48,7 @@ public class OreCluster extends Block{
         super.setBars();
 
         addBar("drilllimit", (OreClusterBuild e) ->
-             new Bar(() -> Core.bundle.format("kratha.drilllimit", drillCount, maxDrillCount), () -> Pal.ammo, () -> ((float)e.drillCount)/e.maxDrillCount));
+             new Bar(() -> Core.bundle.format("kratha.drilllimit", e.drillCount, maxDrillCount), () -> Pal.ammo, () -> ((float)e.drillCount)/maxDrillCount));
     }
 
     @Override

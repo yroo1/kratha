f3d51cc331993a6cc111b4a9175f3ad76f808b60
yroo <purnamaaleron7@gmail.com>
2026-06-30 08:54:24 +0700

    Update KrathaProduction.java

--- src/kratha/content/blocks/KrathaProduction.java
+++ src/kratha/content/blocks/KrathaProduction.java
@@ -69,7 +69,7 @@ public class KrathaProduction {
             consumePower(2/60f);
             consumeLiquid(Liquids.hydrogen, 0.05f).boost();
             
-            drillTime = 22;
+            drillTime = 12;
             drawRim = true;
             hasPower = true;
             tier = 3;

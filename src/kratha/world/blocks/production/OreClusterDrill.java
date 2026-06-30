fa2faa4d6897cc9ce013a2b96bddc533f8420326
yroo <purnamaaleron7@gmail.com>
2026-06-30 19:46:00 +0700

    Update OreClusterDrill.java

--- src/kratha/world/blocks/production/OreClusterDrill.java
+++ src/kratha/world/blocks/production/OreClusterDrill.java
@@ -181,16 +181,14 @@ public class OreClusterDrill extends Block{
 
         @Override
         public boolean onConfigureBuildTapped(Building other){
+            Tile lastOtherTile = world.tile(link);
+            if(lastOtherTile!=null&&lastOtherTile.build!=null&&lastOtherTile.build instanceof OreCluster.OreClusterBuild o)o.updateDrillCount();
             if(other.block instanceof OreCluster&&canLink(other)){
-                Tile lastOtherTile = world.tile(link);
-                if(lastOtherTile!=null&&lastOtherTile.build!=null&&lastOtherTile.build instanceof OreCluster.OreClusterBuild o)o.updateDrillCount();
                 link = other.pos();
                 if(other instanceof OreCluster.OreClusterBuild o)o.updateDrillCount();
                 return false;
             }
             if (world.tile(link)!=null&&(other == this || link == other.pos())){
-                Tile lastOtherTile = world.tile(link);
-                if(lastOtherTile!=null&&lastOtherTile.build!=null&&lastOtherTile.build instanceof OreCluster.OreClusterBuild o)o.updateDrillCount();
                 link = -1;
                 return false;
             }

44fd9bbc588b34882d7e5657e24417ea63beced3
yroo <purnamaaleron7@gmail.com>
2026-07-05 08:25:34 +0700

    Create AncientCrafter.java

---
+++ src/kratha/world/blocks/production/AncientCrafter.java
@@ -0,0 +1,37 @@
+package kratha.world.blocks.production;
+
+import mindustry.gen.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.production.*;
+
+import static mindustry.Vars.state;
+
+public class AncientCrafter extends GenericCrafter{
+    public AncientCrafter(String name){
+        super(name);
+    }
+
+    @Override
+    public boolean canBreak(Tile tile){
+        return state.rules.editor;
+    }
+
+    public class AncientCrafterBuild extends GenericCrafterBuild{
+        
+        @Override
+        public void damage(float damage){
+            return; //no damage
+        }
+
+        @Override
+        public boolean canPickup(){
+            return false; //no
+        }
+
+        @Override
+        public boolean collide(Bullet other){
+            return false; //no
+        }
+    }
+              }
